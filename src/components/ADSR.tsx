07bc1edc0235de5da158037af542b1e8a1a7c289
devonfox <devonhouston.fox@gmail.com>
2023-03-28 18:34:33 -0700

    working my way through param logic

--- src/components/ADSR.tsx
+++ src/components/ADSR.tsx
@@ -1,7 +1,7 @@
 import { animated, useSpring } from "@react-spring/web";
 import Point from "@/components/Point";
 import Line from "@/components/Line";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import EnvSlider from "@/components/Slider";
 import { Box } from "@chakra-ui/react";
 import { Text } from "@chakra-ui/react";
@@ -9,25 +9,33 @@ import { Text } from "@chakra-ui/react";
 const START_X = 10;
 const TOP_Y = 10;
 const BOTTOM_Y = 190;
+const MAX_WIDTH = 400;
+const MAX_HEIGHT = 200;
+const SUSTAIN_LENGTH = 100;
+const END: number = MAX_WIDTH - 10;
+
+const DEFAULT_STATE = {
+  attack: START_X + 20,
+  decay: 55,
+  sustain: 0.5 * BOTTOM_Y,
+  release: 180,
+};
 
 const ADSR = () => {
-  const [attack, setAttack] = useState(START_X);
-  const [decay, setDecay] = useState(55);
-  const [sustain, setSustain] = useState(0.5 * BOTTOM_Y);
+  const [attack, setAttack] = useState(DEFAULT_STATE.attack);
+  const [decay, setDecay] = useState(DEFAULT_STATE.decay);
+  const [sustain, setSustain] = useState(DEFAULT_STATE.sustain);
+  const [release, setRelease] = useState(DEFAULT_STATE.release);
 
   // These are the default constraints for the canvas
   // todo: add accessible svg constraints passed in as props
-
-  // todo: change these values when animated
-  const sustainRel: number = decay + 100;
-  const release: number = 100;
-  const end: number = sustainRel + release;
+  const sustainRel = decay + SUSTAIN_LENGTH;
 
   // todo: make slider values glue together when moving
   // look into use of react-spring here
   const attackSlider = (
     <EnvSlider
-      defaultValue={START_X}
+      defaultValue={DEFAULT_STATE.attack}
       min={START_X}
       max={decay}
       setValue={setAttack}
@@ -36,26 +44,35 @@ const ADSR = () => {
 
   const decaySlider = (
     <EnvSlider
-      defaultValue={55}
+      defaultValue={DEFAULT_STATE.decay}
       min={attack}
-      max={sustain}
+      max={release - SUSTAIN_LENGTH}
       setValue={setDecay}
     />
   );
 
   const sustainSlider = (
     <EnvSlider
-      defaultValue={START_X}
-      min={BOTTOM_Y}
-      max={TOP_Y}
+      defaultValue={DEFAULT_STATE.sustain}
+      min={TOP_Y}
+      max={BOTTOM_Y}
       setValue={setSustain}
+    />
+  );
+
+  const releaseSlider = (
+    <EnvSlider
+      defaultValue={DEFAULT_STATE.release}
+      min={sustainRel}
+      max={END}
+      setValue={setRelease}
     />
   );
 
   return (
     <>
-      <Box ml={20}>
-        <svg height={200} width={400}>
+      <Box ml={10}>
+        <svg height={MAX_HEIGHT} width={MAX_WIDTH}>
           {/*Static Starting Point*/}
           <Point x={START_X} y={BOTTOM_Y} isSolid />
 
@@ -69,34 +86,40 @@ const ADSR = () => {
           <Line start={[decay, sustain]} end={[sustainRel, sustain]} />
 
           {/*Release*/}
-          <Line
-            start={[sustainRel, sustain]}
-            end={[sustainRel + release, BOTTOM_Y]}
-          />
+          <Line start={[sustainRel, sustain]} end={[release, BOTTOM_Y]} />
 
           <Point x={attack} y={TOP_Y} />
           <Point x={decay} y={sustain} />
           <Point x={sustainRel} y={sustain} />
-          <Point x={end} y={BOTTOM_Y} />
+          <Point x={release} y={BOTTOM_Y} />
         </svg>
       </Box>
-      <Box mt={35}>
-        <Text mb={2} fontWeight={"Bold"}>
-          Attack:
-        </Text>
-        {attackSlider}
-      </Box>
-      <Box mt={10}>
-        <Text mb={2} fontWeight={"Bold"}>
-          Decay:
-        </Text>
-        {decaySlider}
-      </Box>
-      <Box mt={10}>
-        <Text mb={2} fontWeight={"Bold"}>
-          Sustain:
-        </Text>
-        {sustainSlider}
+
+      <Box>
+        <Box mt={35}>
+          <Text mb={2} fontWeight={"Bold"}>
+            Attack:
+          </Text>
+          {attackSlider}
+        </Box>
+        <Box mt={10}>
+          <Text mb={2} fontWeight={"Bold"}>
+            Decay:
+          </Text>
+          {decaySlider}
+        </Box>
+        <Box mt={10}>
+          <Text mb={2} fontWeight={"Bold"}>
+            Sustain:
+          </Text>
+          {sustainSlider}
+        </Box>
+        <Box mt={10}>
+          <Text mb={2} fontWeight={"Bold"}>
+            Release:
+          </Text>
+          {releaseSlider}
+        </Box>
       </Box>
     </>
   );
